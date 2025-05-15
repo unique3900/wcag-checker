4027ca5b0065da3b6fff3001b35e3739abb1f395
Parashar Neupane <parashar@tulipstechnologies.com>
2025-05-15 08:16:55 +0545

    update repo

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
+import { Braces } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className=" rounded-md px-4 font-bold max-w-fit mx-auto  w-fit text-white text-center py-4">
-      Designed and Devloped by <Link href="https://github.com/unique3900" className="text-blue-500 hover:text-blue-600" target="_blank">Parashar</Link> 
+    <footer className=" rounded-md px-4 font-bold max-w-fit mx-auto  flex items-center gap-2 w-fit text-white text-center py-4">
+       <Link href="https://github.com/unique3900" className="border border-white hover:border-blue-500 rounded-md p-4 hover:bg-white hover:text-black transition-all duration-300 text-white  flex items-end gap-2" target="_blank"><Braces /> <span className='text-sm'>Contribute on Github</span></Link>
     </footer>
   );
 };
